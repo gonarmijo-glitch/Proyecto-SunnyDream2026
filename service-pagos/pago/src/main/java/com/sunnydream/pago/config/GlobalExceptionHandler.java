808fc3dd15efcffebfa41db122cb7cbbd3d4447f
noemy-navarrete <noem.navarrete@duocuc.cl>
2026-06-17 18:40:55 -0400

    Update GlobalExceptionHandler.java
    Se agrego la clase

--- service-pagos/pago/src/main/java/com/sunnydream/pago/config/GlobalExceptionHandler.java
+++ service-pagos/pago/src/main/java/com/sunnydream/pago/config/GlobalExceptionHandler.java
@@ -1,5 +1,32 @@
 package com.sunnydream.pago.config;
 
-public class GlobalExceptionHandler {
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+@ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> manejarIllegalArgumentException(IllegalArgumentException ex) {
+
+        ErrorResponse error = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> manejarRuntimeException(RuntimeException ex) {
+
+        ErrorResponse error = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 }
