95f31d7a3e5e33bd00e0fd73aa529655ac494828
noemy-navarrete <noem.navarrete@duocuc.cl>
2026-06-17 22:29:02 -0400

    Update ClienteController.java
    Se agrego la anotación '@CrossOrigin(origins = "*")' en la clase ClienteController

--- service-cliente/cliente/src/main/java/com/sunnydream/cliente/controller/ClienteController.java
+++ service-cliente/cliente/src/main/java/com/sunnydream/cliente/controller/ClienteController.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.sunnydream.cliente.model.Cliente;
 import com.sunnydream.cliente.service.ClienteService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/clientes")
 public class ClienteController {
