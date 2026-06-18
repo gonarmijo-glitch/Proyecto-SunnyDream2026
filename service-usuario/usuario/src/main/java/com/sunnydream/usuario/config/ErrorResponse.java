7f433f46e1ae6f9749ee58fbac4127468860a49f
noemy-navarrete <noem.navarrete@duocuc.cl>
2026-06-17 22:19:39 -0400

    Update ErrorResponse.java
    Se creo la clase ErrorResponse en config del Microservicio Usuario

--- service-usuario/usuario/src/main/java/com/sunnydream/usuario/config/ErrorResponse.java
+++ service-usuario/usuario/src/main/java/com/sunnydream/usuario/config/ErrorResponse.java
@@ -1,5 +1,17 @@
 package com.sunnydream.usuario.config;
 
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
+
+    private String mensaje;
+    private int codigo;
+    private LocalDateTime fecha;
 
 }
