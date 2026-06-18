8cf3057fc55e53041e8d32c2b02d5cb046298091
noemy-navarrete <noem.navarrete@duocuc.cl>
2026-06-17 22:13:49 -0400

    Update GlobalExceptionHandler.java
    se creo la clase GlobalExceptionHandler

--- service-cliente/cliente/src/main/java/com/sunnydream/cliente/config/GlobalExceptionHandler.java
+++ service-cliente/cliente/src/main/java/com/sunnydream/cliente/config/GlobalExceptionHandler.java
@@ -1,5 +1,22 @@
 package com.sunnydream.cliente.config;
 
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> manejarRuntimeException(RuntimeException ex) {
+
+        ErrorResponse error = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 }
