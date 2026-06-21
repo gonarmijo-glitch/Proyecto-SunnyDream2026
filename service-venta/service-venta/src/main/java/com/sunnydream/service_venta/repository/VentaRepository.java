2f1bb36c7d7edd9d0e688a23c8b07d45477ed50f
Gonzalo Armijo <gon.armijo@duocuc.cl>
2026-06-21 02:13:17 -0400

    Delete service-venta/service-venta directory

--- service-venta/service-venta/src/main/java/com/sunnydream/service_venta/repository/VentaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sunnydream.service_venta.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sunnydream.service_venta.model.Venta;
-
-public interface VentaRepository extends JpaRepository<Venta, Long> {
-}
