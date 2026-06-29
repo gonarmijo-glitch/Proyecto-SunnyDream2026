06f81c4e304ce096a55eab5608e4f5967d6cb24d
Valentina González Salinas <vd.gonzalezs@duocuc.cl>
2026-06-28 20:21:52 -0400

    Update CarritoService.java

--- carrito/carrito/src/main/java/com/sunnydream/carrito/service/CarritoService.java
+++ carrito/carrito/src/main/java/com/sunnydream/carrito/service/CarritoService.java
@@ -1,7 +1,7 @@
 package com.sunnydream.carrito.service;
 
 import java.util.List;
-
+import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
