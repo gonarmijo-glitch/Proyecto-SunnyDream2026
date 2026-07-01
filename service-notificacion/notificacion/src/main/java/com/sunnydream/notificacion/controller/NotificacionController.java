933952c598d5110dbdbd8b10a71cfafefc2b4866
Gonzalo Armijo <gon.armijo@duocuc.cl>
2026-06-30 22:22:26 -0400

    Se arregla la sintaxis de notificaciones controler

--- service-notificacion/notificacion/src/main/java/com/sunnydream/notificacion/controller/NotificacionController.java
+++ service-notificacion/notificacion/src/main/java/com/sunnydream/notificacion/controller/NotificacionController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/notificaciones")
 public class NotificacionController {
-    
+    @CrossOrigins(origins="*"
     @Autowired
     private NotificacionService notificacionService;
 
